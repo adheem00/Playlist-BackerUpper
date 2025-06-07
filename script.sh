51e680c9d7026f5e9f6cc3eec2e723816bf037e5
Adheem <154681917+adheem00@users.noreply.github.com>
2025-06-07 15:50:36 +0100

    Update script.sh
    Edited the process of mapping song names to table, then adding them to USB Drive. Also modified file paths to cater to my new computer.

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# /mnt/c/Users/zizan/Downloads/code/bash/projects
+# /mnt/c/Users/adhee/Downloads/Programming/Code/Bash/Projects
 
 # Written by Adheem Khan
 # 15/5/25 - 18/5/25
@@ -10,20 +10,21 @@
 # Copy any music files in my Playlist folder into my USB (provided they arent already there).
 # Delete any music files in my USB that arent in the Playlist folder.
 
-sudo mount -t drvfs D: /mnt/d
 
-PLAYLIST_PATH="/mnt/c/Users/zizan/Downloads/Playlist"
-EXTERNAL_STORAGE_PATH="/mnt/d"
+sudo mount -t drvfs D: /mnt/d/
 
-DELETION_HISTORY="/mnt/c/Users/zizan/Downloads/deletionhistory.txt"
-ADDITION_HISTORY="/mnt/c/Users/zizan/Downloads/additionhistory.txt"
+PLAYLIST_PATH="/mnt/c/Users/adhee/Downloads/Music/Playlist"
+EXTERNAL_STORAGE_PATH="/mnt/d/BackupPlaylist"
 
-tracks_to_be_deleted=("song1" "song2")
-tracks_to_update_to_drive=("song1" "song2")
+DELETION_HISTORY="/mnt/c/Users/adhee/Downloads/Music/deletionhistory.txt"
+ADDITION_HISTORY="/mnt/c/Users/adhee/Downloads/Music/additionhistory.txt"
+
+tracks_to_be_deleted=()
+tracks_to_update_to_drive=()
 
 
 # Removing Files from DriveKnight
-mapfile -t tracks_to_be_deleted < <(time diff -rq $PLAYLIST_PATH $EXTERNAL_STORAGE_PATH | grep "Only in /mnt/d: " | \
+mapfile -t tracks_to_be_deleted < <(time diff -rq $PLAYLIST_PATH $EXTERNAL_STORAGE_PATH | grep "Only in /mnt/d/BackupPlaylist:" | \
 grep -v "System Volume Information" | cut --complement -d' ' -f1-3)
 
 for track in "${tracks_to_be_deleted[@]}"; do
@@ -37,19 +38,23 @@ echo "All inconsistent files have been removed from DriveKnight."
 echo "Beginning to update files to DriveKnight..."
 
 # Adding Files TO DriveKnight
-mapfile -t tracks_to_update_to_drive < <(time diff -rq $PLAYLIST_PATH $EXTERNAL_STORAGE_PATH | \
-grep "Only in /mnt/c/Users/zizan/Downloads/Playlist: " | grep -v "System Volume Information" | cut --complement -d' ' -f1-3)
-
-printf '%s\n' "${tracks_to_update_to_drive[@]}"
+mapfile -t tracks_to_update_to_drive < <(
+    diff -rq "$PLAYLIST_PATH" "$EXTERNAL_STORAGE_PATH" |
+    grep "Only in $PLAYLIST_PATH:" |
+    grep -v "System Volume Information" |
+    sed "s|Only in $PLAYLIST_PATH: ||"
+)
 
 for track in "${tracks_to_update_to_drive[@]}"; do
-    echo "$track is about to be added to DriveKnight!"
+    echo "$track is being added to DriveKnight!"
     
     echo "$track" >> $ADDITION_HISTORY
     cp "$PLAYLIST_PATH/$track" "$EXTERNAL_STORAGE_PATH"
 done
 
 # Final diff command to check for any anomalies
-diff -rq "$PLAYLIST_PATH" "$EXTERNAL_STORAGE_PATH" | grep -Ev "System Volume Information|sync\.ffs_db|Only in .*/Album Covers"
+diff -rq "$PLAYLIST_PATH" "$EXTERNAL_STORAGE_PATH" | grep -Ev "System Volume Information|sync\.ffs_db"
+
+echo "Done!"
 
 
